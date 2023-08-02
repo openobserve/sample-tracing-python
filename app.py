63cdea4e59e5c5f71ac5880fb1137282ca183840
omkarK06 <127078473+omkarK06@users.noreply.github.com>
2023-08-02 10:49:44 +0530

    add: added users, posts routes and requestInstrumentor

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 import tracing
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
-from flask import Flask
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
 FlaskInstrumentor.instrument_app(app)
+RequestsInstrumentor().instrument()  # This line instruments requests
 
 @app.route("/")
 def hello():
     return 'Hello World!'
+
+@app.route("/posts")
+def posts():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    posts = response.json()
+    comments()
+    return jsonify(posts)
+
+@app.route("/users")
+def users():
+    response = requests.get('https://jsonplaceholder.typicode.com/users')
+    posts = response.json()
+    return jsonify(posts)
+
+def comments():
+    requests.get('https://jsonplaceholder.typicode.com/comments')
 
 app.run(debug=True)
